30f789773bf05481969324806814b663af1db8e2
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-01-09 08:34:57 +0200

    create Student Controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+	public function index()
+	{
+		return view('student.index');
+	}
+
+	public function create()
+	{
+	}
+
+	public function store(Request $request)
+	{
+	}
+
+	public function show($id)
+	{
+	}
+
+	public function edit($id)
+	{
+	}
+
+	public function update(Request $request, $id)
+	{
+	}
+
+	public function destroy($id)
+	{
+	}
+}
