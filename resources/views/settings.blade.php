f8081af9f904e5a2356498319250ad31c19b9c29
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-01-10 05:38:32 +0200

    create settings blade php

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,30 @@
+@push('css')
+    <style>
+    </style>
+@endpush
+
+@push('js')
+
+@endpush
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <i class="fa-solid fa-gears mr-1"></i>
+            {{ __('Settings') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3>
+                        <i class="fa-solid fa-gears mr-1"></i>
+                        {{ __('Settings') }}
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
