485a615740963b2b443e48d415e2e6bd3bfa9f12
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-01-27 06:50:47 +0200

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,6 @@
 
     @include('admin.nav.admin-nav')
 
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -33,6 +32,5 @@
             </div>
         </div>
     </div>
-
 
 </x-app-layout>
